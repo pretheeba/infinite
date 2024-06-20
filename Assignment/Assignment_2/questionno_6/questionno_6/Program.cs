c17ff7c2ab799f90983d6df2af4e5fc57c69520d
Pretheeba Udhayakumar <pretheebau@infinite.com>
2024-06-19 23:09:36 -0700

    Question 6

---
+++ Assignment/Assignment_2/questionno_6/questionno_6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace questionno_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word :");
+            string word = Console.ReadLine();
+            int count = 0;
+
+            for (int i =0;i<word.Length;i++)
+            {
+                
+                count++;
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
