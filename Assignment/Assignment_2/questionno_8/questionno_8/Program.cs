fc4aec57b99e0860127b33f0945b70714d56fe8c
Pretheeba Udhayakumar <pretheebau@infinite.com>
2024-06-19 23:38:50 -0700

    Question 8

--- Assignment/Assignment_2/questionno_8/questionno_8/Program.cs
+++ Assignment/Assignment_2/questionno_8/questionno_8/Program.cs
@@ -4,26 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace questionno_7
+namespace questionno_8
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Enter a word :");
-            string  first_word = Console.ReadLine();
+            string first_word = Console.ReadLine();
             Console.WriteLine("Enter second word :");
             string second_word = Console.ReadLine();
 
-            for (int i=0; i<first_word.Length;i++ )
+            for (int i = 0; i < first_word.Length; i++)
             {
-                if(first_word[i] != second_word[i])
+                if (first_word[i] != second_word[i])
                 {
                     Console.WriteLine("Both are different");
                     Console.ReadLine();
                 }
-                
-                
+
+
             }
             Console.WriteLine(" both are Same");
             Console.ReadLine();
