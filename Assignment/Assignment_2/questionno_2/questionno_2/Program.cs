1795992c15f68c0cb57e3d78e7b91dc4e26f6b8d
Pretheeba Udhayakumar <pretheebau@infinite.com>
2024-06-19 21:22:31 -0700

    Question_2

---
+++ Assignment/Assignment_2/questionno_2/questionno_2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace questionno_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number : ");
+            int num = int.Parse(Console.ReadLine());
+
+            switch(num)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    Console.ReadLine();
+                    break;
+
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    Console.ReadLine();
+
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    Console.ReadLine();
+
+                    break;
+                case 4:
+                    Console.WriteLine("Thrusday");
+                    Console.ReadLine();
+
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    Console.ReadLine();
+
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    Console.ReadLine();
+
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    Console.ReadLine();
+
+                    break;
+
+                default:
+                    Console.WriteLine("Enter a valid number");
+                    Console.ReadLine();
+                    break;
+
+
+
+            }
+        }
+    }
+}
