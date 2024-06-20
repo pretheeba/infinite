e5e5e8366f127776af3151c9663a05d7ddbb96f3
Pretheeba Udhayakumar <pretheebau@infinite.com>
2024-06-19 21:08:50 -0700

    Question_1

---
+++ Assignment/Assignment_2/questionno_1/questionno_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace questionno_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number :");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number :");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = num1 + num2;
+
+            if(num1 == num2)
+            {
+                sum *= 3;
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
