c007c0482584ac888456d88f2d441f737b00b5a4
Pretheeba Udhayakumar <pretheebau@infinite.com>
2024-07-20 11:30:13 +0530

    Assignemnt _04

---
+++ ADO/Disconnected_Eg1/Disconnected_Eg1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Data;
+using System;
+using System.Data.SqlClient;
+
+namespace Disconnected_Eg1
+{
+    class Program
+    {
+        public static SqlConnection con = null;
+        public static SqlDataAdapter da;
+        static void Main(string[] args)
+        {
+            Disconnected_approach();
+            Console.Read();
+        }
+
+        public static void Disconnected_approach()
+        {
+            con = new SqlConnection("data source=ICS-LT-98M46D3; initial catalog=northwind;" + " user id=sa; password=pretheeba;");
+
+            con.Open();
+            da = new SqlDataAdapter("select * from Region", con);
+            DataSet ds = new DataSet();
+
+            da.Fill(ds, "NorthwindRegion");
+            DataTable dt = ds.Tables["NorthwindRegion"];
+
+            //to access the rows and columns from the datatable
+            foreach (DataRow drow in dt.Rows)
+            {
+                foreach (DataColumn dcol in dt.Columns)
+                {
+                    Console.Write(drow[dcol]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
