5a158102de6fcbfd16b262903c6ae20b72083b76
Pretheeba Udhayakumar <pretheebau@infinite.com>
2024-07-22 13:26:25 +0530

    Assignment_05

--- C#/Hands-On/basic_03/basic_03/Program.cs
+++ C#/Hands-On/basic_03/basic_03/Program.cs
@@ -4,64 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Day1_Dotnet
+namespace Day11_Dotnet
 {
-    class Static_functions_Eg
+    class Program
     {
-        //fields
-        static int data = 5;
-        int x;
-        public static void Main()
+        static void Main(string[] args)
         {
-            //char c; //one byte is allocated
-            //int x; //4 bytes are allocated
-            //float f; //8 bytes are allloted
 
-            //creating and instantiating an object
-            Static_functions_Eg sfe = new Static_functions_Eg();
-            Console.WriteLine(sfe.Add(10, 12));
-            Console.WriteLine("{0}, {1}", data, sfe.x);
-            sfe.x = 15;
-            Console.WriteLine("{0}, {1}", data, sfe.x);
-            //let us create one more object
-            Static_functions_Eg sfe1 = new Static_functions_Eg();
-            Console.WriteLine(sfe1.Add(15, 15));
-            data = 50;
-            sfe1.x = 25;
-            Console.WriteLine("{0}, {1}", data, sfe1.x);
+            Console.WriteLine("The number of Generations : " + GC.MaxGeneration);
+            Program p = new Program();
 
-            Console.WriteLine("------------");
-            Console.WriteLine("{0}, {1}", data, sfe.x);
-            Console.WriteLine("{0}, {1}", data, sfe1.x);
-            // OddNos(5);
-            // OnlyStatic.Evennos(5);
-            Console.ReadKey();
-        }
+            Console.WriteLine("The Gen number of the Object P is " + GC.GetGeneration(p));
+            Console.WriteLine("The Total Memory : " + GC.GetTotalMemory(true));
+            GC.Collect(0);
+            Console.WriteLine("Garbage collection in Gen 0 : " + GC.CollectionCount(0));
 
-        //instance function
-        public int Add(int x, int y)
-        {
-            return x + y;
-        }
+            Console.Read();
 
-        //static function also known as class function
-        public static void OddNos(int x)
-        {
-            if (x % 2 != 0)
-                Console.WriteLine("Number is Odd");
-            else
-                Console.WriteLine("Number is Even");
-        }
-    }
-
-    class OnlyStatic
-    {
-        public static void Evennos(int x)
-        {
-            if (x % 2 == 0)
-                Console.WriteLine("Number is Even");
-            else
-                Console.WriteLine("Number is not even");
         }
     }
 }
